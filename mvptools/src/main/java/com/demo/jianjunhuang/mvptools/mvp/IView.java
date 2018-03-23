da5705f5f4ea2424525f048ade25e963fec13268
jiangjunhuang <demon903732218@gmail.com>
2018-03-23 16:46:57 +0800

    add module and default img

---
+++ mvptools/src/main/java/com/demo/jianjunhuang/mvptools/mvp/IView.java
@@ -0,0 +1,10 @@
+package com.demo.jianjunhuang.mvptools.mvp;
+
+/**
+ * @author jianjunhuang.me@foxmail.com
+ * @since 2017/5/24.
+ */
+
+public interface IView {
+
+}
---
+++ mvptools/src/main/java/com/demo/jianjunhuang/mvptools/mvp/impl/BaseModel.java
@@ -0,0 +1,15 @@
+package com.demo.jianjunhuang.mvptools.mvp.impl;
+
+import com.demo.jianjunhuang.mvptools.mvp.IModel;
+
+/**
+ * @author jianjunhuang.me@foxmail.com
+ * @since 2017/5/24.
+ */
+
+public class BaseModel implements IModel {
+
+  @Override public void onDestroy() {
+
+  }
+}
